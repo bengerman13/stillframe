d46b1972e3a2f666ec5d49a9bfaa14944f422bf8
Ben Berry <bengerman@gmail.com>
2024-11-29 19:22:53 -0500

    FEATURE: Track image metadata
    - use right mime type for image
    - provide client with image metadata (to prepare for admin client)
    - refactor ConnectionManager a little

--- stillframe/connection_manager.py
+++ stillframe/connection_manager.py
@@ -9,28 +9,31 @@ class ConnectionManager:
     def __init__(self, image_source: FileWalkerImageSource):
         self.image_source = image_source
         self.active_connections: list[WebSocket] = []
-        self.current_image: bytes = self.build_message()
+        self.current_still = self.get_next_still()
 
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
         self.active_connections.append(websocket)
-        await websocket.send_bytes(self.current_image)
+        await self.send_still(websocket)
 
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
 
-    async def broadcast(self, message: bytes):
-        for connection in self.active_connections:
-            try:
-                await connection.send_bytes(message)
-            except WebSocketDisconnect:
-                self.disconnect(connection)
+    async def send_still(self, websocket: WebSocket):
+        try:
+            await websocket.send_text(self.current_still.to_json())
+        except WebSocketDisconnect:
+            self.disconnect(websocket)
 
-    def build_message(self):
-        return self.image_source.get_next_still()
+    async def broadcast_still(self):
+        for connection in self.active_connections:
+            await self.send_still(connection)
+
+    def get_next_still(self):
+        self.current_still = self.image_source.get_next_still()
 
     async def serve_forever(self):
         while True:
-            self.current_image = self.build_message()
-            await self.broadcast(self.current_image)
+            self.get_next_still()
+            await self.broadcast_still()
             await asyncio.sleep(1)
--- stillframe/image_source.py
+++ stillframe/image_source.py
@@ -1,6 +1,38 @@
+import base64
+import json
 import pathlib
 
+from dataclasses import dataclass, asdict
+
 from stillframe.config import Config
+
+
+def mime_for_extension(extension: str) -> str:
+    if extension == "jpg":
+        extension = "jpeg"
+    return f"image/{extension}"
+
+
+@dataclass
+class Still:
+    id_: str
+    mime_type: str
+    is_denylisted: bool
+    source: str
+    image_data: str
+
+    def to_json(self) -> str:
+        d = asdict(self)
+        d.pop("source")
+        return json.dumps(d)
+
+    def hydrate(self):
+        with open(self.source, "rb") as f:
+            b64 = base64.b64encode(f.read()).decode("utf-8")
+            self.image_data = f"data:{self.mime_type};base64,{b64}"
+
+    def dehydrate(self):
+        self.image_data = ""
 
 
 class FileWalkerImageSource:
@@ -8,19 +40,36 @@ class FileWalkerImageSource:
     def __init__(self, source_path: pathlib.Path, extensions: list[str]):
         self.path = source_path
         self.extensions = extensions
-        self.stills = []
+        self.stills: list[Still] = []
+        self.stills_by_id = {}
         self.rescan()
         self.current_still_index = 0
-        self.still_metadata = {}
 
     def rescan(self):
         stills = []
         for extension in self.extensions:
-            stills.extend(self.path.glob(f"**/*.{extension}"))
-        self.stills = sorted(stills)
+            mime = mime_for_extension(extension)
+            for path in self.path.glob(f"**/*.{extension}"):
+                still = Still(
+                    # TODO: use something other than path as id
+                    id_=str(path),
+                    mime_type=mime,
+                    is_denylisted=False,
+                    source=path,
+                    image_data="",
+                )
+                if still.id_ not in self.stills_by_id:
+                    stills.append(still)
+                    self.stills_by_id[still.id_] = still
+        self.stills.extend(stills)
+        self.stills.sort(key=lambda x: x.source)
         self.current_still_index = 0
 
-    def get_next_still(self):
-        with open(self.stills[self.current_still_index], "rb") as f:
+    def get_next_still(self) -> (Still, bytes):
+        last_still = self.stills[self.current_still_index]
+        self.current_still_index += 1
+        while (next_still := self.stills[self.current_still_index]).is_denylisted:
             self.current_still_index += 1
-            return f.read()
+        next_still.hydrate()
+        last_still.dehydrate()
+        return next_still
